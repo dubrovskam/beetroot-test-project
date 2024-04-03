46248571bc1024909c4370a4db60a066c514eac3
dubrovskam <mdubrovskaa116@gmail.com>
2024-04-03 14:08:46 +0300

    edit to main index.js

--- javascript/index.js
+++ javascript/index.js
@@ -4,13 +4,13 @@ import objects from './js/objects.js';
 
 // import array from './js/array.js';
 
-import domPlayList from './js/dom-playlist.js';
+// import domPlayList from './js/dom-playlist.js';
 
-import asyncJS from './js/acync-js.js';
+// import asyncJS from './js/acync-js.js';
 
 
 objects();
 // functions();
 // array();
-domPlayList();
-asyncJS();
+// domPlayList();
+// asyncJS();
